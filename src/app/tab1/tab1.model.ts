9dcf48cd9df3491f65dbd48803bf7a6fd85ee54e
Vimal <vsvimal444@gmail.com>
2021-12-01 21:51:25 +0530

    Event Page UI Completed

---
+++ src/app/tab1/tab1.model.ts
@@ -0,0 +1,30 @@
+export const PROFILE_ICON =
+	'https://png.pngtree.com/png-clipart/20190924/original/pngtree-business-people-avatar-icon-user-profile-free-vector-png-image_4815126.jpg';
+
+export const REGISTERED_DONOR_LIST = [
+	{
+		user_name: 'Vimal V S',
+		user_location: 'Kerala',
+		profile_icon: PROFILE_ICON,
+	},
+	{
+		user_name: 'Vaibhav Goel',
+		user_location: 'Delhi',
+		profile_icon: PROFILE_ICON,
+	},
+	{
+		user_name: 'Pranay Kothari',
+		user_location: 'Kolkata',
+		profile_icon: PROFILE_ICON,
+	},
+	{
+		user_name: 'Ashwani G',
+		user_location: 'Kerala',
+		profile_icon: PROFILE_ICON,
+	},
+	{
+		user_name: 'Bala S W',
+		user_location: 'Kerala',
+		profile_icon: PROFILE_ICON,
+	},
+];
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { PROFILE_ICON, REGISTERED_DONOR_LIST } from './tab1.model';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+	selector: 'app-tab1',
+	templateUrl: 'tab1.page.html',
+	styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+	registeredDonorList = REGISTERED_DONOR_LIST;
+	profileIcon = PROFILE_ICON;
+	constructor() {}
 }
